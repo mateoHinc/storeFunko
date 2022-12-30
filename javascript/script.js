51f870f755904bff5bad461c5f35c3638f11bbe5
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2022-12-30 17:10:08 -0500

    Jquery

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,29 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+$('#sendComment').on('show.bs.modal', function(e) {
+    console.log('Mostrando modal');
+    $('#commentBtn').removeClass('btn-outline-info');
+    $('#commentBtn').addClass('btn-success');
+    $('#commentBtn').prop('disabled',true);
+});
+
+
+/*$('#sendComment').on('shown.bs.modal', function(e) {
+    console.log('Se mostró modal');
+    $('#commentBtn').removeClass('btn-success');
+    $('#commentBtn').addClass('btn-warning');
+});*/
+
+
+$('#sendComment').on('hide.bs.modal', function(e) {
+    console.log('El modal contacto se oculta');
+});
+
+$('#sendComment').on('hidden.bs.modal', function(e) {
+    console.log('El modal contacto se ocultó');
+    $('#commentBtn').removeClass('btn-success');
+    $('#commentBtn').addClass('btn-outline-info')
+    /*$('#commentBtn').prop('disabled', false);*/
+});
